d0a3a63f6d423c97cd3659c542de3e6ea3717530
Rob Oman <rob.oman@icloud.com>
2022-05-18 22:08:38 +0100

    Fixes CORS issue

--- server/app.js
+++ server/app.js
@@ -4,12 +4,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const functionsRouter = require('./routes/functions');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
